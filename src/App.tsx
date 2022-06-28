fdb2e12db8f7e2b667eac2617d7ca39192e637f8
IlyaMoroz92 <93884456+IlyaMoroz92@users.noreply.github.com>
2022-06-28 21:31:03 +0300

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 
 import './App.scss';
+import { Button } from './components/Button'
+import { ReactComponent as Pokeball } from './assets/img/pokeball.svg'
+import { ReactComponent as Up} from './assets/img/Up.svg'
 
 function App() {
 
   return (
     <div className="App">
-      Hello
+      <Button
+        className="with-icon"
+        icon={<Up />}
+      />
+      <Button
+        className="with-icon"
+        icon={<Pokeball />}
+      />
     </div>
   );
 }
