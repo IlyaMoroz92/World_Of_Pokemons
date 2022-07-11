1ec3af025212550a0849715dcd8edc19d99e702d
IlyaMoroz92 <93884456+IlyaMoroz92@users.noreply.github.com>
2022-07-11 20:26:29 +0300

    feat: refactor

--- src/components/ToolsBar/Toolsbar.tsx
+++ src/components/ToolsBar/Toolsbar.tsx
@@ -1,32 +1,105 @@
+import axios from 'axios'
 import { Input } from '../Input'
 import './Toolsbar.scss'
+import { useEffect, useState } from 'react';
+import { Card } from '../Card';
+
 
 type ToolsbarProps = {
     className?: string
 }
 
+type Data = {
+    id: number
+    name: string
+    sprites?: any
+    types: any
+}
+
+type PokemonSearch = {
+    data: Data
+}
+
 export const Toolsbar = (props: ToolsbarProps) => {
+
+
+
+    const [pokemon, setPokemon] = useState('pikachu')
+    const [pokemonData, setPokemonData] = useState<[Data]>()
+    const [pokemonType, setPokemonType] = useState('')
+
+    const getSearchPokemon = async () => {
+        
+
+        
+
+        const toArray: any  = []
+        try {
+            const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+            const res: PokemonSearch = await axios.get(url)
+            toArray.push(res.data)
+            setPokemonType(res.data.types[0].type.name)
+            setPokemonData(toArray)
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const handleChange = (e: any) => {
+        setPokemon(e.target.value.toLowerCase())
+    }
+
+    const  handleSubmit = ( e: any) => {
+        e.preventDefault();
+        getSearchPokemon()
+    }
+
+    console.log(pokemonData);
     
     return (
+        <div className='toolsbar__main'>
         <div className= {`toolsbar toolsbar--${props.className}`}>
             <div className="search">
-                <Input
-                    className='toolsbar'
-                    type='search'
-                    placeholder='🔍 Search...'
-                />
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <Input
+                            onChange={handleChange}
+                            className='toolsbar'
+                            type='text'
+                            placeholder=' 🔍 Search...'
+                        />
+                    </label>
+                </form>
+                
             </div>
             <div className="sort">
             <select className='toolsbar__select'> Sort
                 <option value="id">Id</option>
+                <option value="id">Name</option>
                 <option value="weight">Weight</option>
                 <option value="height">Height</option>
                 <option value="base_experience">Experience</option>
-                <option value="Hp ">Attack</option>
-                <option value="Hp ">Defense </option>
-                <option value="Hp ">Speed </option>
             </select>
             </div>
+        </div>
+                    <div className='search__result'>
+                    {
+                    pokemonData?.map((data) => {
+                        return(
+                            <Card
+                                titleId={true}
+                                likebar={true}
+                                nameId={true}
+                                id={data.id}
+                                name={data.name}
+                                sprites={data.sprites.other.dream_world.front_default}
+                                key={data.id}
+                            />
+                            )
+                    })
+                    }
+                </div> 
         </div>
     )
 }
--- src/features/getPokemons/pokemonsSlice.ts
+++ src/features/getPokemons/pokemonsSlice.ts
@@ -11,13 +11,18 @@ interface IPokemonsState {
     pokemonsInfo: IPokemon[] | null,
     isLoading: 'idle' | 'pending',
     error: string | null,
+    isFavoritePokemon: number[]
+    isPokeballPokemon: number[]
 }
+
 
 const initialState: IPokemonsState = {
     content: null,
     isLoading: 'idle',
     error: null,
     pokemonsInfo: null,
+    isFavoritePokemon: [],
+    isPokeballPokemon: []
 }
 
 export const pokemonsSlice = createSlice({
@@ -41,7 +46,10 @@ export const pokemonsSlice = createSlice({
         fetchPokemonsInfoSuccess:(state, action:PayloadAction<Array<IPokemon>>) => {
             if(state.isLoading === 'pending'){
                 state.isLoading = 'idle'
-                state.pokemonsInfo = action.payload
+                state.pokemonsInfo = action.payload.map(pokemon => state.isFavoritePokemon.some(id =>  id === pokemon.id) ? {...pokemon, like: true} : pokemon)
+                if(state.pokemonsInfo) {
+                    state.pokemonsInfo = state.pokemonsInfo.map(pokemon => state.isPokeballPokemon.some(id =>  id === pokemon.id) ? {...pokemon, pokeball: true} : pokemon)
+                }
             }
         },
         fetchPokemonsInfoFailure: (state, action: PayloadAction<string>) => {
@@ -49,7 +57,7 @@ export const pokemonsSlice = createSlice({
             state.error = action.payload
             
         },
-        likePokemon: (state, action: PayloadAction<number>) => {
+        /* likePokemon: (state, action: PayloadAction<number>) => {
             if(state.pokemonsInfo) {
                 state.pokemonsInfo = state.pokemonsInfo.map(pokemon => pokemon.id === action.payload ? {...pokemon, like: !pokemon.like ? true : undefined} : pokemon)
             }
@@ -58,6 +66,31 @@ export const pokemonsSlice = createSlice({
             if(state.pokemonsInfo) {
                 state.pokemonsInfo = state.pokemonsInfo.map(pokemon => pokemon.id === action.payload ? {...pokemon, pokeball: !pokemon.pokeball} : pokemon)
             }
+        }, */
+        likePokemon: (state, action: PayloadAction<number>) => {
+            if(state.pokemonsInfo) {
+                if(state.isFavoritePokemon.some(id => id === action.payload)) {
+                    state.isFavoritePokemon = state.isFavoritePokemon.filter(id => id !== action.payload)
+                } else {
+                    state.isFavoritePokemon = [action.payload, ...state.isFavoritePokemon]
+                }
+                if(state.pokemonsInfo) {
+                    
+                    state.pokemonsInfo = state.pokemonsInfo.map(pokemon => state.isFavoritePokemon.some(id => id === pokemon.id) ? {...pokemon, like: true} : {...pokemon, like: false} )
+                }
+                
+            }
+        },
+        pokeballPokemon: (state, action: PayloadAction<number>) => {
+                if(state.isPokeballPokemon.some(id => id === action.payload)) {
+                    state.isPokeballPokemon = state.isPokeballPokemon.filter(id => id !== action.payload)
+                } else {
+                    state.isPokeballPokemon = [action.payload, ...state.isPokeballPokemon]
+                }
+                if(state.pokemonsInfo) {
+                    
+                    state.pokemonsInfo = state.pokemonsInfo.map(pokemon => state.isPokeballPokemon.some(id => id === pokemon.id) ? {...pokemon, pokeball: true} : {...pokemon, pokeball: false} )
+                }
         },
     },
 })
